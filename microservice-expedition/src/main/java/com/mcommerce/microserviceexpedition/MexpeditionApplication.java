b3e87a9e63e02be37c2d6b997a301f7d2f4f4403
godzyken <isgodzy@msn.com>
2018-09-11 14:52:55 +0200

    test pour expedition
    Signed-off-by: godzy <isgodzy@msn.com>

--- microservice-expedition/src/main/java/com/mcommerce/microserviceexpedition/MexpeditionApplication.java
+++ microservice-expedition/src/main/java/com/mcommerce/microserviceexpedition/MexpeditionApplication.java
@@ -2,12 +2,10 @@ package com.mcommerce.microserviceexpedition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableDiscoveryClient
+@EnableFeignClients("com.mexpedition")
 public class MexpeditionApplication {
 
 	public static void main(String[] args) {
